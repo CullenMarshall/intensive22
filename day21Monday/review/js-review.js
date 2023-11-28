be316a7c7cdc028a8c806ecd370170b59e30ca19
Cullen <bongsooma@gmail.com>
2023-11-28 10:04:18 +0900

    Monday Objects practice

---
+++ day21Monday/review/js-review.js
@@ -0,0 +1,204 @@
+// =========== Functions & Conditionals =========== 
+
+// 1. Check if a number is positive, negative, or zero.
+
+// Steps:
+//       - Create a function named checkNumber with num as parameter
+//       - Inside the function create an if else statement using
+//         num. 
+//       - If the number is grater than zero, print to the 
+//          console "The number is positive."
+//       - Else if the number is less than zero, print
+//         to the console "The number is negative"
+//       - Else console "The number is zero"
+//       - Last, call the function and pass any number as the arguement.
+
+
+{
+   // YOUR CODE HERE:
+
+
+}
+
+// 2. Check if a number is even or odd. 
+
+// Steps:
+//       - Create a function named checkEvenOdd with num as parameter
+//       - Inside the function create an if else statement using
+//         num. 
+//       - If the number is even console `The number ${num} is even
+//       - Else console `The number ${num} is odd
+//       - Last, call the function and pass any number as the arguement.
+
+{
+   // YOUR CODE HERE:
+
+
+}
+
+// 3. Determine the largest of three numbers.
+// Steps:
+//       - Create a function named findLargest with a, b, c as parameters
+//       - Inside the function create an if else statement using
+//         a, b, c. 
+//       - If a is the largest, console a
+//       - Else if b is the largest, console b  
+//       - Else if c is the largest, console c  
+//       - Last, call the function and pass any number as the arguement
+{
+   // YOUR CODE HERE:
+
+
+}
+
+// 4. Check if a string is empty and console true if is empty 
+//    or false if it's not empty
+
+//    Now use same staps as the previous exercises.
+//    Create a function, check for the if else
+//    Call the function
+//    Remember to include the PARAMETER when you create the function
+//    And pass the ARGUMENT when you call the function
+{
+   // YOUR CODE HERE:
+
+}
+
+// 5. Check if a number is divisible by both 5 AND 7.
+//    Console true  if it is or false if it's not
+//    Create function, check for condition, call function.
+{
+   // YOUR CODE HERE:
+
+
+}
+
+// 6. Determine if a person is eligible to vote (considering a voting age of 18).
+//    "Eligible to vote" if it's true / "Not eligible to vote" if it's false
+{
+   // YOUR CODE HERE:
+
+
+}
+
+// 7. Determine the season based on the month (spring, summer, fall, winter).
+//    - Create a function named getSeason and console log the season depending 
+//       on the month you pass when you call the function.
+{
+   // YOUR CODE HERE:
+
+
+}
+
+// 8. Determine the discount percentage based on the purchase amount:
+//     10% for amounts less than $100
+//     20% for amounts between $100 and $500
+//     30% for amounts greater than $500
+//
+//     Console the amout of the discount.
+
+{
+   // YOUR CODE HERE:
+
+
+}
+
+// 9. Determine if a given day is a weekday or weekend.
+
+//p.s. Pass as argument the day of the week, i.e Monday 
+//     and check if it's a weekday or weekend
+
+{
+   // YOUR CODE HERE:
+
+
+}
+
+
+// ========== FOR LOOPS ===========
+
+// EXERCISE 1:
+{
+   // Use a for loop to print numbers from 1 to 5.
+   // The loop initializes i to 1, 
+   // iterates as long as i is less than or equal to 5, 
+   // increments i by 1 in each iteration, 
+   // and prints the value of i to the console.
+
+}
+
+
+// EXERCISE 2:
+{
+   // Use a for loop to print even numbers from 2 to 10.
+   // Similar to the previous example, 
+   // but the loop increments i by 2 in each iteration, 
+   // ensuring only even numbers are printed.
+
+
+}
+
+
+// EXERCISE 3
+{
+   // Use a for loop to print the square of numbers from 1 to 5.
+
+
+}
+
+
+// EXERCISE 4
+{
+   // Use a for loop to print "Hello" five times.
+
+
+}
+
+
+// EXERCISE 5
+{
+   // Use a for loop to print numbers in reverse order from 5 to 1.
+
+
+}
+
+
+// EXERCISE 6
+{
+   // Use a for loop to calculate and print the sum of numbers from 1 to 5.
+
+
+}
+
+
+
+// EXERCISE 7
+{
+   // Use a for loop to print numbers from 1 to 10 that are divisible by 3.
+
+
+}
+
+
+// EXERCISE 8
+{
+   // Use a for loop to calculate and print the average of numbers from 1 to 5.
+
+
+}
+
+// EXERCISE 9
+{
+   // . Use for loops to print the following pattern to the console:
+   // *
+   // **
+   // ***
+   // ****
+   // *****
+
+
+}
+
+
+// TRY THE LOOPS EXERCISES WITH DIFFERENT LOOPS!!! :)
+
